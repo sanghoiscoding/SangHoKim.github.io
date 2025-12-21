b13331792f9be3e4426f948de461b67dabac96c0
sanghoiscoding <sanghoisworking@gmail.com>
2025-12-21 17:05:12 +0000

    Deploying to gh-pages from @ sanghoiscoding/About_SangHo@6486ae48caa6207ac0fa4ed4b563c574e42f74b0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "sanghoiscoding/SanghoKim.github.io",
+    "data-repo": "sanghoiscoding/About_SangHo",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
